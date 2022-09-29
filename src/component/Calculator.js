d9c1b587803a91b3e73a750a5fc74b65c297dde8
Mohamad Hormaty <mamalyhormaty2012@gmail.com>
2022-09-30 00:26:55 +0330

    implement structure of operators

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { BtnsData } from '../data/BtnsData';
+import React, { useState } from 'react';
+import { btnsData } from '../data/BtnsData';
 
 const Calculator = () => {
+  const [curState, setCurState] = useState('');
+  const [input, setInput] = useState(0);
+
+  const handleClick = (e, action) => {
+    switch (action) {
+      case 'numbers':
+        return console.log('numbers');
+      case 'operators':
+        return console.log('operators');
+      case 'reset':
+        return console.log('reset');
+      case 'minusPlus':
+        return console.log('minusPlus');
+      case 'percent':
+        return console.log('percent');
+      case 'equal':
+        return console.log('equal');
+      default:
+        return;
+    }
+  };
+
   return (
     <div className="container">
-      <div className="screen">0</div>
-      {BtnsData.map((btn, idx) => (
-        <div key={idx} className={`btn ${btn.class}`} onClick={btn.action}>
+      <div className="screen">{input}</div>
+      {btnsData.map((btn, idx) => (
+        <div key={idx} className={`btn ${btn.class}`} onClick={(e) => handleClick(e, btn.action)}>
           {btn.content}
         </div>
       ))}
